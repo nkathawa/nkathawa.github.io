031e1058bca88fe82e0ade4a35d4c9f7265b1df6
Navin Kathawa <nkathawa@mac-r9m74wcd9m.attlocal.net>
2024-04-11 15:47:10 -0400

    improvements

--- finance-blog/src/Contact.js
+++ finance-blog/src/Contact.js
@@ -3,24 +3,22 @@ import './css/Contact.css';
 
 function Contact() {
   return (
-    <>
-      <div>
-          <div className="container">
-              <form action="https://formspree.io/nkathawa@umich.edu" method="POST">
-                  <label htmlFor="fname">Full Name</label>
-                  <input type="text" id="fname" name="fullname" placeholder="Your full name"/>
-                  <label htmlFor="email">Email</label>
-                  <input type="text" id="email" name="email" placeholder="Your email"/>
-                  <label htmlFor="subject">Subject</label>
-                  <input type="text" id="subject" name="subject" placeholder="Subject line"/>
-                  <label htmlFor="message">Message</label>
-                  <textarea id="message" name="message" placeholder="Write something..."></textarea>
-                  <input type="text" name="_gotcha" className="gotcha"/>
-                  <input type="submit" value="Submit"/>
-              </form>
-          </div>
-      </div>
-    </>
+    <div>
+        <div className="container">
+            <form action="https://formspree.io/nkathawa@umich.edu" method="POST">
+                <label htmlFor="fname">Full Name</label>
+                <input type="text" id="fname" name="fullname" placeholder="Your full name"/>
+                <label htmlFor="email">Email</label>
+                <input type="text" id="email" name="email" placeholder="Your email"/>
+                <label htmlFor="subject">Subject</label>
+                <input type="text" id="subject" name="subject" placeholder="Subject line"/>
+                <label htmlFor="message">Message</label>
+                <textarea id="message" name="message" placeholder="Write something..."></textarea>
+                <input type="text" name="_gotcha" className="gotcha"/>
+                <input type="submit" value="Submit"/>
+            </form>
+        </div>
+    </div>
   );
 }
 
