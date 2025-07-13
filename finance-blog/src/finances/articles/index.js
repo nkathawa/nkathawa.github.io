8c8b1529969d7ac1098be13b0658030919247a7b
Navin Kathawa <nkathawa@umich.edu>
2025-07-13 11:27:57 -0400

    delete a lot of stuff

--- finance-blog/src/finances/articles/index.js
+++
@@ -1,7 +0,0 @@
-import budgeting101 from './budgeting101';
-
-const articles = {
-    budgeting101: { component: budgeting101, title: 'Budgeting 101', date: 'April 12, 2024', excerpt: 'I firmly believe that a budget is the foundation of good finances. I highly recommend starting any financial journey with a sound budget, as it provides [...]' },
-};
-
-export default articles;
